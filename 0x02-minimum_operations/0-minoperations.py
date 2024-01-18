cdae45f2efd2e71c6c6f9f59250da5b7a42216f2
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-01-18 20:15:02 +0000

    removed condition

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,17 +6,14 @@ def minOperations(n):
     """Returns the min num of operations to
     result exactly in n times H chars"""
 
-    if type(n) is not int or n < 2:
-        return 0
-
     facts = [0]
     x = n
     i = n - 1
 
     while i > 0:
         if x % i == 0:
-            facts.append(int(x/i))
+            facts.append(x/i)
             x = i
         i -= 1
 
-    return sum(facts)
+    return int(sum(facts))
