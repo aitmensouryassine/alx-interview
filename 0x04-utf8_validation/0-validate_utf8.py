dfebdd0bb57fc235010ab838e3ef8adc9f415bae
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-01-31 19:02:10 +0000

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,27 @@
 
 
 def validUTF8(data):
-    if len(data) == 1 and data[0] <= 127:
-        return True
-    return False
+    i = j = 0
+
+    while i < len(data):
+        bi = format(data[i], '08b')
+
+        if bi.startswith("110"):
+            j = 1
+        elif bi.startswith("1110"):
+            j = 2
+        elif bi.startswith("11110"):
+            j = 3
+        else:
+            i += 1
+
+        while j:
+            i += 1
+            if i >= len(data):
+                return False
+            bi = format(data[i], '08b')
+            if not bi.startswith('10'):
+                return False
+            j -= 1
+
+    return True
